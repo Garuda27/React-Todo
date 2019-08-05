9f5290d878cb602167ed6f1b28d3958dbad10448
Preston <pres.10.nap@gmail.com>
2019-08-05 17:24:49 -0400

    have the idea down I think

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ToDo = props => {
+    return (
+        <div onClick = {() => props.toggleTask(props.task.id)}>
+            <p>{props.item.task}</p>
+    </div>
+    )
+}
+export default ToDo
